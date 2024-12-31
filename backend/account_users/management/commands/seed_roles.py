0ab60d04c7bfa1f3c906ddfef53048bc46ed6e74
BrookTeferi <brookteferi1q2w@gmail.com>
2024-12-31 15:43:33 +0300

    feat: update seed roles command with new role names

--- backend/account_users/management/commands/seed_roles.py
+++ backend/account_users/management/commands/seed_roles.py
@@ -5,7 +5,7 @@ class Command(BaseCommand):
     help = "Seed initial roles"
 
     def handle(self, *args, **kwargs):
-        roles = ["Medical Student", "Educator", "Institution"]
+        roles = ["Student", "Instructor", "Institution"]
         for role in roles:
             Role.objects.get_or_create(name=role)
         self.stdout.write(self.style.SUCCESS("Roles seeded successfully!"))
